00280e51397a439c18d0c1f0a91a17a29fb57568
leon matias <leonmatias1991@gmail.com>
2020-04-15 23:45:06 -0500

    add change

--- COURSE02/springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
+++ COURSE02/springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
@@ -6,10 +6,11 @@ import com.formacionbdi.springboot.app.commons.models.entity.Producto;
 
 public interface IProductoService {
 
-	public List<Producto> findAll();
-	public Producto findById(Long id);
+	List<Producto> findAll();
+
+	Producto findById(Long id);
 	
-	public Producto save(Producto producto);
+	Producto save(Producto producto);
 	
-	public void deleteById(Long id);
+	void deleteById(Long id);
 }
